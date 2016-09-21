1f48ffe92e11eb7aa0dce6fda296d9c0af6c5526
Wliu <Wliu1402@gmail.com>
2016-09-20 22:02:47 -0400

    Store valid keys in a list

--- src/input_handler.py
+++ src/input_handler.py
@@ -4,22 +4,37 @@ Definition for input handler, which captures keystrokes.
 
 from tkinter import *
 
-
 class InputHandler:
     def __init__(self):
         root = Tk()
 
+        keys = list()
+
         def onKeyPressed(event):
-            if event.keysym.startswith("Shift"):  # Shift_L, Shift_R
-                print("hoho!")
+            # Ignore non-printable characters
+            # This does not capture Tab, BackSpace, Return, Escape
+            if(event.char == ""):
                 return
 
-            print("{} was pressed".format(event.keysym))
+            if(event.keysym == "Escape"):
+                for key in keys:
+                    print(key)
+                keys.clear()
+                return
+
+            # keysym gives the actual key for special characters
+            # eg Ctrl_L for Control
+            # We're ignoring them right now (see above), but it could be useful
+            # in the future.
+            # Otherwise it should be identical to char.
+            keys.append(event.keysym)
 
         frame = Frame(root, width=100, height=100)
         frame.bind("<Key>", onKeyPressed)
         frame.pack()
         frame.focus_set()
+
+        print("Listening for keys...press Esc to list detected keys")
 
         root.mainloop()
 
