39ee0cf35a8d7f2fd1237f982c8b1d20bd32e67f
Wliu <Wliu1402@gmail.com>
2016-09-20 21:22:01 -0400

    Experiment with tkinter

--- src/input_handler.py
+++ src/input_handler.py
@@ -2,6 +2,25 @@
 Definition for input handler, which captures keystrokes.
 """
 
+from tkinter import *
+
+
 class InputHandler:
     def __init__(self):
-        pass
+        root = Tk()
+
+        def onKeyPressed(event):
+            if event.keysym.startswith("Shift"):  # Shift_L, Shift_R
+                print("hoho!")
+                return
+
+            print("{} was pressed".format(event.keysym))
+
+        frame = Frame(root, width=100, height=100)
+        frame.bind("<Key>", onKeyPressed)
+        frame.pack()
+        frame.focus_set()
+
+        root.mainloop()
+
+test = InputHandler()
