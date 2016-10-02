d7caba1ace97c5cb5cd15e9a7c70d5a8c8a6ad5c
William Li <wzl7zb@virginia.edu>
2016-10-02 12:15:15 -0400

    Update game_state.py
    it's a little messy right now, but i'm thinking that each individual user can have multiple save files in a single GameState object that can be initialized at the beginning of the game

--- src/game_state.py
+++ src/game_state.py
@@ -1,21 +1,54 @@
-"""
-Definition of game state, which is the model in our MVC framework.
-"""
+#Definition of game state, which is the model in our MVC framework.
+
 import json
 
 class GameState:
 
-    def __init__(self):
-        GameState = []
-        GameState['user'] = {
-            'name': 'user',
-            'address': 'home',
-            'gender': 'male'
+    name = ""
+    gender = ""
+    location = ""
+    id_number = 0;
+
+    inventory = [] #should be a dictionary object
+    status = []
+    playtime = 0 #minutes
+
+    def __init__(self): #more instance variables can be added and subtracted
+        GameState = {
+            {
+                'name': "John",
+                'gender': "Male",
+                'location': "Home",
+                'id_number': 0,
+
+                'playtime': 50,
+                'inventory': ["spoon", "trowel", "flashlight"],
+                'status': "asleep",
+            }
         }
 
         s = json.dumps(GameState)
-        with open("C://User//GameState.txt", "w") as f:
+        with open("C://User//GameState.txt", "w") as f: #write a different filepath
             f.write(s)
+        pass
 
+    def add(state): #may not be the correct syntax; depending on how the file is read, each individual "state"
+                    #on someone's computer should have their own 'GameState object' that can be added to
+        GameState.update(state)
 
-            pass
+        loads()
+        pass
+
+    def loads(): #when any instance is added to GameState, this method is called to create another saveFile
+        with open ("C://User//GameState.txt") as json_data: #write  a different filepath
+            save_data = json.loads(json_data)
+
+        for individual_state in save_data['GameState']
+            fo = open(individual_state['id'] + "_" + ['name'] + ".txt", "wb")
+
+            for item in individual_state:
+                fo.write(item + "           ")
+
+            fo.close()
+
+        pass
