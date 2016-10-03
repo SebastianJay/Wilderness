6ef43b68a7ea63bc88fb05ed6e68a8a7beafd86a
jackbprescott <jackbprescott@gmail.com>
2016-10-03 00:30:05 -0400

    Made tuples instead of lists for immutable data
    Also took out animation method, to be done manually in main loop

--- src/loading_window.py
+++ src/loading_window.py
@@ -1,42 +1,56 @@
-import window
-import time
+from window import Window
+from time import *
 
 
-class loading_window(window):
+class LoadingWindow(window):
 
-    def __init__(self, w, h, updateDelay, border):
+    def __init__(self, w, h, border):
 
         self.width = w
         self.height = h
         self.windowChars = []
-        self.continueAnimating = True
-        self.delay = updateDelay
         self.border = border
+		self.animState = 1
 
-        self.animStateOne = [["L", [self.width // 2 - 5, self.height // 2]], ["o", [self.width // 2 - 4, self.height // 2]],
-                             ["a", [self.width // 2 - 3, self.height // 2]], ["d", [self.width // 2 - 2, self.height // 2]],
-                             ["i", [self.width // 2 - 1, self.height // 2]], ["n", [self.width // 2, self.height // 2]],
-                             ["g", [self.width // 2 + 1, self.height // 2]], [".", [self.width // 2 + 2, self.height // 2]],
-                             [" ", [self.width // 2 + 3, self.height // 2]], [" ", [self.width // 2 + 4, self.height // 2]], ]
-        self.animStateTwo = [["L", [self.width // 2 - 5, self.height // 2]], ["o", [self.width // 2 - 4, self.height // 2]],
-                             ["a", [self.width // 2 - 3, self.height // 2]], ["d", [self.width // 2 - 2, self.height // 2]],
-                             ["i", [self.width // 2 - 1, self.height // 2]], ["n", [self.width // 2, self.height // 2]],
-                             ["g", [self.width // 2 + 1, self.height // 2]], [".", [self.width // 2 + 2, self.height // 2]],
-                             [".", [self.width // 2 + 3, self.height // 2]], [" ", [self.width // 2 + 4, self.height // 2]], ]
-        self.animStateThree = [["L", [self.width // 2 - 5, self.height // 2]], ["o", [self.width // 2 - 4, self.height // 2]],
-                               ["a", [self.width // 2 - 3, self.height // 2]], ["d", [self.width // 2 - 2, self.height // 2]],
-                               ["i", [self.width // 2 - 1, self.height // 2]], ["n", [self.width // 2, self.height // 2]],
-                               ["g", [self.width // 2 + 1, self.height // 2]], [".", [self.width // 2 + 2, self.height // 2]],
-                               [".", [self.width // 2 + 3, self.height // 2]], [".", [self.width // 2 + 4, self.height // 2]], ]
+        self.animStateOne = [("L", (self.width // 2 - 5, self.height // 2)),
+                             ("o", (self.width // 2 - 4, self.height // 2)),
+                             ("a", (self.width // 2 - 3, self.height // 2)),
+                             ("d", (self.width // 2 - 2, self.height // 2)),
+                             ("i", (self.width // 2 - 1, self.height // 2)), 
+							 ("n", (self.width // 2, self.height // 2)),
+                             ("g", (self.width // 2 + 1, self.height // 2)),
+                             (".", (self.width // 2 + 2, self.height // 2)),
+                             (" ", (self.width // 2 + 3, self.height // 2)),
+                             (" ", (self.width // 2 + 4, self.height // 2))]
+        self.animStateTwo = [("L", (self.width // 2 - 5, self.height // 2)),
+                             ("o", (self.width // 2 - 4, self.height // 2)),
+                             ("a", (self.width // 2 - 3, self.height // 2)),
+                             ("d", (self.width // 2 - 2, self.height // 2)),
+                             ("i", (self.width // 2 - 1, self.height // 2)), 
+							 ("n", (self.width // 2, self.height // 2)),
+                             ("g", (self.width // 2 + 1, self.height // 2)),
+                             (".", (self.width // 2 + 2, self.height // 2)),
+                             (".", (self.width // 2 + 3, self.height // 2)),
+                             (" ", (self.width // 2 + 4, self.height // 2))]
+        self.animStateThree = [("L", (self.width // 2 - 5, self.height // 2)),
+                               ("o", (self.width // 2 - 4, self.height // 2)),
+                               ("a", (self.width // 2 - 3, self.height // 2)),
+                               ("d", (self.width // 2 - 2, self.height // 2)),
+                               ("i", (self.width // 2 - 1, self.height // 2)),
+                               ("n", (self.width // 2, self.height // 2)),
+                               ("g", (self.width // 2 + 1, self.height // 2)),
+                               (".", (self.width // 2 + 2, self.height // 2)),
+                               (".", (self.width // 2 + 3, self.height // 2)),
+                               (".", (self.width // 2 + 4, self.height // 2))]
 
-        for i in range(0, self.height):
+        for i in range(self.height):
             self.windowChars.append([])
 
-            for j in range(0, self.width):
+            for j in range(self.width):
                 self.windowChars[i].append(" ")
 
         if self.border:
-            for i in range(0, self.width):
+            for i in range(self.width):
                 self.windowChars[0][i], self.windowChars[self.height - 1][i] = "*", "*"
 
             for i in range(1, self.height - 1):
@@ -49,10 +63,12 @@ class loading_window(window):
         Creates a border of asterisks around the window
         """
         for i in range(0, self.width):
-            self.windowChars[0][i], self.windowChars[self.height - 1][i] = "*", "*"
+            self.windowChars[0][i] = "*",
+			self.windowChars[self.height - 1][i] = "*"
 
         for i in range(1, self.height - 1):
-            self.windowChars[i][0], self.windowChars[i][self.width - 1] = "*", "*"
+            self.windowChars[i][0] = "*"
+			self.windowChars[i][self.width - 1] = "*"
 
 
     def draw(self):
@@ -66,26 +82,22 @@ class loading_window(window):
         print()
 
 
-    def update(self, listOfUpdates):
+    def update(self):
         """
-        listOfUpdates to be in form [ [charcter, [self.widthOfChar, self.heightOfChar]], ...]
-        delay to be in milliseconds
-        """
-        time.sleep(self.delay / 1000)
-        for i in listOfUpdates:
-            self.windowChars[int(i[1][1])][int(i[1][0])] = i[0]
-        self.draw()
-
-    def animate(self):
-
-        while self.continueAnimating:
-            self.update(self.animStateOne)
-            self.update(self.animStateTwo)
-            self.update(self.animStateThree)
-
-    def stopAnimating(self):
-
-        self.continueAnimating = False
+		Updates loading window
+		"""
+		if self.animState == 1:
+        	for i in animStateOne:
+            	self.windowChars[int(i[1][1])][int(i[1][0])] = i[0]
+			animState = 2
+		elif self.animState == 2:
+			for i in animStateTwo:
+            	self.windowChars[int(i[1][1])][int(i[1][0])] = i[0]
+			animState = 3	
+		else:
+			for i in animStateThree:
+            	self.windowChars[int(i[1][1])][int(i[1][0])] = i[0]
+			animState = 1
 
 
 	
