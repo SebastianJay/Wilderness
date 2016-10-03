8bebc3d54d6d48ea0397c644c9324dfd965fcd55
jackbprescott <jackbprescott@gmail.com>
2016-10-03 00:02:15 -0400

    Made animation instructions self contained

--- src/loading_window.py
+++ src/loading_window.py
@@ -1,19 +1,48 @@
 import window
-import window_draw
 import time
 
+
 class loading_window(window):
-    def __init__(self, w, h):
+
+    def __init__(self, w, h, updateDelay, border):
 
         self.width = w
         self.height = h
         self.windowChars = []
+        self.continueAnimating = True
+        self.delay = updateDelay
+        self.border = border
+
+        self.animStateOne = [["L", [self.width // 2 - 5, self.height // 2]], ["o", [self.width // 2 - 4, self.height // 2]],
+                             ["a", [self.width // 2 - 3, self.height // 2]], ["d", [self.width // 2 - 2, self.height // 2]],
+                             ["i", [self.width // 2 - 1, self.height // 2]], ["n", [self.width // 2, self.height // 2]],
+                             ["g", [self.width // 2 + 1, self.height // 2]], [".", [self.width // 2 + 2, self.height // 2]],
+                             [" ", [self.width // 2 + 3, self.height // 2]], [" ", [self.width // 2 + 4, self.height // 2]], ]
+        self.animStateTwo = [["L", [self.width // 2 - 5, self.height // 2]], ["o", [self.width // 2 - 4, self.height // 2]],
+                             ["a", [self.width // 2 - 3, self.height // 2]], ["d", [self.width // 2 - 2, self.height // 2]],
+                             ["i", [self.width // 2 - 1, self.height // 2]], ["n", [self.width // 2, self.height // 2]],
+                             ["g", [self.width // 2 + 1, self.height // 2]], [".", [self.width // 2 + 2, self.height // 2]],
+                             [".", [self.width // 2 + 3, self.height // 2]], [" ", [self.width // 2 + 4, self.height // 2]], ]
+        self.animStateThree = [["L", [self.width // 2 - 5, self.height // 2]], ["o", [self.width // 2 - 4, self.height // 2]],
+                               ["a", [self.width // 2 - 3, self.height // 2]], ["d", [self.width // 2 - 2, self.height // 2]],
+                               ["i", [self.width // 2 - 1, self.height // 2]], ["n", [self.width // 2, self.height // 2]],
+                               ["g", [self.width // 2 + 1, self.height // 2]], [".", [self.width // 2 + 2, self.height // 2]],
+                               [".", [self.width // 2 + 3, self.height // 2]], [".", [self.width // 2 + 4, self.height // 2]], ]
 
         for i in range(0, self.height):
             self.windowChars.append([])
 
             for j in range(0, self.width):
                 self.windowChars[i].append(" ")
+
+        if self.border:
+            for i in range(0, self.width):
+                self.windowChars[0][i], self.windowChars[self.height - 1][i] = "*", "*"
+
+            for i in range(1, self.height - 1):
+                self.windowChars[i][0], self.windowChars[i][self.width - 1] = "*", "*"
+
+
 
     def border(self):
         """
@@ -25,6 +54,7 @@ class loading_window(window):
         for i in range(1, self.height - 1):
             self.windowChars[i][0], self.windowChars[i][self.width - 1] = "*", "*"
 
+
     def draw(self):
         """
         Displays the windowChars matrix using print()
@@ -35,33 +65,27 @@ class loading_window(window):
                 print(self.windowChars[i][j], end="")
         print()
 
-    def update(self, listOfUpdates, delay):
+
+    def update(self, listOfUpdates):
         """
-        listOfUpdates to be in form [ [charcter, [widthOfChar, heightOfChar]], ...]
+        listOfUpdates to be in form [ [charcter, [self.widthOfChar, self.heightOfChar]], ...]
         delay to be in milliseconds
         """
-        time.sleep(delay/1000)
+        time.sleep(self.delay / 1000)
         for i in listOfUpdates:
             self.windowChars[int(i[1][1])][int(i[1][0])] = i[0]
         self.draw()
 
-"""
-The below code is suggestion/example to demonstrate how to use the methods to
-create a good looking loading box, and shows what it would look like
-"""
+    def animate(self):
 
-width = 51
-height = 11
+        while self.continueAnimating:
+            self.update(self.animStateOne)
+            self.update(self.animStateTwo)
+            self.update(self.animStateThree)
 
-loadingWindow = loading_window(width,height)
-loadingWindow.border()
+    def stopAnimating(self):
 
-animStateOne = [["L", [width//2-5,height//2]], ["o", [width//2-4,height//2]], ["a", [width//2-3,height//2]], ["d", [width//2-2,height//2]], ["i", [width//2-1,height//2]], ["n", [width//2,height//2]], ["g", [width//2+1,height//2]], [".", [width//2+2,height//2]], [" ", [width//2+3,height//2]], [" ", [width//2+4,height//2]], ]
-animStateTwo =[["L", [width//2-5,height//2]], ["o", [width//2-4,height//2]], ["a", [width//2-3,height//2]], ["d", [width//2-2,height//2]], ["i", [width//2-1,height//2]], ["n", [width//2,height//2]], ["g", [width//2+1,height//2]], [".", [width//2+2,height//2]], [".", [width//2+3,height//2]], [" ", [width//2+4,height//2]], ]
-animStateThree = [["L", [width//2-5,height//2]], ["o", [width//2-4,height//2]], ["a", [width//2-3,height//2]], ["d", [width//2-2,height//2]], ["i", [width//2-1,height//2]], ["n", [width//2,height//2]], ["g", [width//2+1,height//2]], [".", [width//2+2,height//2]], [".", [width//2+3,height//2]], [".", [width//2+4,height//2]], ]
+        self.continueAnimating = False
 
-for i in range(1, 30):
-    loadingWindow.update(animStateOne, 500)
-    loadingWindow.update(animStateTwo, 500)
-    loadingWindow.update(animStateThree, 500)
+
 	
