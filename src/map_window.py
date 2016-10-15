c31504997be94e12754fda4bdee91fff0af7e6df
Wliu <Wliu1402@gmail.com>
2016-10-15 18:48:01 -0400

    Get the draw method working

--- src/map_window.py
+++ src/map_window.py
@@ -10,12 +10,27 @@ class MapWindow(Window):
         super().__init__(width, height)
         loader = AssetLoader()
         assets = loader.loadAssets('assets/maps')
+        self.map = assets[currentMap].splitlines()
 
     def update(self, timestep, keypresses):
-        pass
+        for key in keypresses:
+            if key == "Up" or key == "w":
+                pass
+            elif key == "Left" or key == "a":
+                pass
+            elif key == "Down" or key == "s":
+                pass
+            elif key == "Right" or key == "d":
+                pass
+            elif key == "Return":
+                pass
 
     def draw(self):
-        pass
+        for row in range(len(self.map)):
+            for column in range(len(self.map[row])):
+                self.pixels[row][column] = self.map[row][column]
+        return self.pixels
 
 if __name__ == '__main__':
-    mapWindow = MapWindow('test_map.txt', 1280, 720)
+    mapWindow = MapWindow('assets\\maps\\test_map.txt', 1280, 720)
+    mapWindow.draw()
