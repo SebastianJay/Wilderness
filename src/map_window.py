47bd65617492b9b131c72b0b84131336a0701b2a
Wliu <Wliu1402@gmail.com>
2016-10-15 18:13:27 -0400

    Some more MapWindow work

--- src/map_window.py
+++ src/map_window.py
@@ -3,13 +3,19 @@ Window containing the world map.
 """
 from window import Window
 from game_state import GameState
+from asset_loader import AssetLoader
 
 class MapWindow(Window):
-    def __init__(self, width, height):
+    def __init__(self, currentMap, width, height):
         super().__init__(width, height)
+        loader = AssetLoader()
+        assets = loader.loadAssets('assets/maps')
 
     def update(self, timestep, keypresses):
         pass
 
     def draw(self):
         pass
+
+if __name__ == '__main__':
+    mapWindow = MapWindow('test_map.txt', 1280, 720)
