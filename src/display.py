7cced13ae323b2a17b9919ec7545039c9964e3f1
cwarthur <cwa3bj@virginia.edu>
2016-11-04 23:14:51 -0400

    Working on displaying font colors

--- src/display.py
+++ src/display.py
@@ -20,16 +20,50 @@ class Display:
             padx=0, pady=0, bd=0, selectbackground='black')
         self.text.pack()
 
+
     def draw(self):
         pixels = self.windowManager.draw()
         bufferlst = []
+
+        # formatting = self.windowManager._formatting
+        # _formatting: [(string tag, (int start_index, int end_index)), (... )]
+        # bold, italics, underline, and text color
+
         for sublst in pixels:
             substr = ''.join(sublst)
             bufferlst.append(substr)
         bufferstr = '\n'.join(bufferlst)
+
         self.text.config(state=tk.NORMAL)
         self.text.delete(1.0, tk.END)
-        self.text.insert(tk.END, bufferstr)
+
+        start_index = 0
+        for subformat in self.windowManager._formatting:   # subformat is one tuple: (string tag, (int start_index, int end_index))
+            plain_text = bufferstr[start_index:subformat[1][0]]     # all the text between the last formatter and the start of this formatter
+            formatted_text = bufferstr[subformat[1][0]:(subformat[1][1]+1)]   # all the text contained within this formatter
+
+            formatter = subformat[0]
+            is_bold = 0
+            is_italicized = 0
+            is_underlined = 0
+            formats = formatter.split('_')
+            for format in formats:
+                color = "white"
+                if format == "bold":
+                    is_bold = 1
+                elif format == "italic":
+                    is_italicized = 1
+                elif format == "underline":
+                    is_underlined = 1
+                else:
+                    color = format
+                self.text.tag_config(formatter, foreground=color) # bold=is_bold, italic=is_italicized, underline=is_underlined
+            self.text.insert(tk.END, plain_text)
+            self.text.insert(tk.END, formatted_text, formatter)
+            start_index = subformat[1][1] + 1   # start next search after this formatter
+
+        if start_index < len(bufferstr):
+            self.text.insert(tk.END, bufferstr[start_index:])
         self.text.config(state=tk.DISABLED)
 
     def getWidget(self):
